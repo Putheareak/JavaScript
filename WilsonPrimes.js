bc9456a47df23f1030dc31b4d3b823e8d37422fd
Putheareak <45678262+Putheareak@users.noreply.github.com>
2019-03-19 23:10:01 +0700

    Update WilsonPrimes.js

--- WilsonPrimes.js
+++ WilsonPrimes.js
@@ -11,3 +11,6 @@ function amIWilson(p) {
 Logic of wilson prime is : ((n-1)!+1)%(nxn)==0;
 Step 1 : finding the fuctorial of (p-1)!
 Step 2 : using the ternary operator to return true or false
+Try to differenciate between "true" and true becasue :
+true : it will return as it a key word;
+"true" : it will return a it a string;
